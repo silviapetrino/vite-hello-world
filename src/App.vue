87c41a6d5539d809cf71ba67d59531b6657e7ae6
silviapetrino <petrino.silvia@gmail.com>
2023-10-09 14:09:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+<script>
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    name: "app",
+    components: {
+      AppTitle
+    }
+  }
+</script>
+
+
+<template>
+  <AppTitle />
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+  export default {
+    name: "AppTitle"
+  }
+
+</script>
+
+<template>
+  <h1>La mia prima app con Vite!</h1>
+  
+</template>
+
+
+<style>
+
+</style>
